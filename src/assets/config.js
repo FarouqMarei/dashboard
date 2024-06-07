e9af9fc834c7a55d6c8d007c03937e6739422f32
Farouq <farouq.marei@gmail.com>
2024-06-08 01:16:38 +0300

    config

---
+++ src/assets/config.js
@@ -0,0 +1,7 @@
+apiUrl: 'http://localhost:8080/api';
+
+homeApi: 'Controller/v1/API/';
+eventsApi: 'Controller/v1/API/';
+realTimeApi: 'Controller/v1/API/';
+domainApi: 'Controller/v1/API/';
+devicesApi: 'Controller/v1/API/';
